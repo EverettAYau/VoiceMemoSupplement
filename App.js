2c1d431bdc7c4e1d656f5c5b0ba1395cc67d76f3
EverettYau <73495043+EverettYau@users.noreply.github.com>
2024-04-11 10:39:49 -0700

    Commit 1
    Started implementing a way to upload Voice Memos to the app.

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DocumentPicker from 'react-native-document-picker'
+import ImportButton from './components/ImportButton';
+
 
 export default function App() {
+  const files = {}
+  function importFiles() {
+    files.add(pickSingle(allowMultiSelection))
+  }
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
